c89f31936724ba624c995983ac6d97a278483d4c
Serhii Steanchuk <stepanchuk.dev@gmail.com>
2024-05-02 11:19:00 +0300

    Add ESLint configuration files and update dependencies

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,20 +1,28 @@
-/* eslint-disable @typescript-eslint/naming-convention */
 import globals from 'globals'
-
 import path from 'path'
 import { fileURLToPath } from 'url'
 import { FlatCompat } from '@eslint/eslintrc'
 import pluginJs from '@eslint/js'
 
 // mimic CommonJS variables -- not needed if using CommonJS
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
-const compat = new FlatCompat({ baseDirectory: __dirname, recommendedConfig: pluginJs.configs.recommended })
+const _filename = fileURLToPath(import.meta.url)
+const _dirname = path.dirname(_filename)
+const compat = new FlatCompat({ baseDirectory: _dirname, recommendedConfig: pluginJs.configs.recommended })
 
 export default [
   { languageOptions: { globals: globals.node } },
   ...compat.extends('standard-with-typescript'),
-  {
-    files: ['src/**/*.ts', 'src/**/*.tsx']
-  }
+  ...compat.config({
+    rules: {
+      '@typescript-eslint/space-before-function-paren': 'off'
+    },
+    parserOptions: {
+      project: './tsconfig.eslint.json'
+    },
+    overrides: [
+      {
+        files: ['src/**/*.ts']
+      }
+    ]
+  })
 ]
